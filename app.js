57a2032d80f24e48536b53e742fabbbb41fd8367
vladblk <blankvladp@gmail.com>
2020-06-18 11:27:32 +0300

    refactoring and showing searched pictures

--- app.js
+++ app.js
@@ -8,70 +8,53 @@ let searchValue;
 // EVENT LISTENERS
 
 searchInput.addEventListener('input', updateInput);
-searchForm.addEventListener('click', (e) => {
+searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchPhoto(searchValue);
 });
 
 // FUNCTIONS
-async function curatedPhotos(){
-  const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+async function fetchApi(url){
+  const dataFetch = await fetch(url, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       Authorization: auth
     }
   });
-  const data = await dataFetch.json();
 
+  const data = await dataFetch.json();
+  
   data.photos.forEach( (photo) => {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
     galleryImg.innerHTML = `
-      <img src=${photo.src.large}></img>
-      <p>${photo.photographer}</p>
+      <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href="${photo.src.original}">Download</a>
+      </div>
+      <img src=${photo.src.large}>
     `
     gallery.appendChild(galleryImg);
-    // const galleryImg = `
-    //   <div class="gallery-img">
-    //     <img src="${photo.src.large}"></img>
-    //     <p>${photo.photographer}</p>
-    //   </div>
-    // `
-    // gallery.insertAdjacentHTML('beforeend', galleryImg);
   });
+}
+
+async function curatedPhotos(){
+  fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
 };
 curatedPhotos();
 
 async function searchPhoto(query){
-  const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      Authorization: auth
-    }
-  });
-
-  const data = await dataFetch.json();
-  data.photos.forEach( (photo) => {
-    const galleryImg = document.createElement('div');
-    galleryImg.classList.add('gallery-img');
-    galleryImg.innerHTML = `
-      <img src=${photo.src.large}></img>
-      <p>${photo.photographer}</p>
-    `
-    gallery.appendChild(galleryImg);
-    // const galleryImg = `
-    //   <div class="gallery-img">
-    //     <img src="${photo.src.large}"></img>
-    //     <p>${photo.photographer}</p>
-    //   </div>
-    // `
-    // gallery.insertAdjacentHTML('beforeend', galleryImg);
-  });
+  clear();
+  fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`);
 };
-
 
 function updateInput(e){
   searchValue = e.target.value;
+}
+
+function clear(){
+  gallery.innerHTML = '';
+  searchInput.value = '';
+  searchInput.focus();
 }
