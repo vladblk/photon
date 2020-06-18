4a47dbb68b415ae8fd734e61669fac1a103fabed
vladblk <blankvladp@gmail.com>
2020-06-18 13:32:40 +0300

    load more pictures on more button click

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const auth = '563492ad6f917000010000017f4d29b69f194a7787c072439d164e23';
 const searchInput = document.querySelector('.search-input');
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.more-btn');
 
 let searchValue;
+let pageCount = 1;
+let fetchLink;
+let currentSearch;
 
 // EVENT LISTENERS
 
@@ -11,7 +15,10 @@ searchInput.addEventListener('input', updateInput);
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchPhoto(searchValue);
+
+  currentSearch = searchValue;
 });
+moreBtn.addEventListener('click', loadMore);
 
 // FUNCTIONS
 async function fetchApi(url){
@@ -25,6 +32,38 @@ async function fetchApi(url){
 
   const data = await dataFetch.json();
   
+  return data;
+}
+
+async function curatedPhotos(){
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
+  generatePhotos(data);
+};
+curatedPhotos();
+
+async function searchPhoto(query){
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  generatePhotos(data);
+};
+
+async function loadMore(){
+  pageCount++;
+
+  if(currentSearch){
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${pageCount}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${pageCount}`;
+  }
+
+  const data = await fetchApi(fetchLink);
+
+  generatePhotos(data);
+}
+
+function generatePhotos(data){
   data.photos.forEach( (photo) => {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
@@ -38,16 +77,6 @@ async function fetchApi(url){
     gallery.appendChild(galleryImg);
   });
 }
-
-async function curatedPhotos(){
-  fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
-};
-curatedPhotos();
-
-async function searchPhoto(query){
-  clear();
-  fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`);
-};
 
 function updateInput(e){
   searchValue = e.target.value;
